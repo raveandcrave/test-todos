0d11b52235f4445622d4239a6282ad7ca0912f86
Viktor Kopan <victorkopan98@gmail.com>
2023-09-17 18:11:49 +0600

    added persist

--- src/hooks/useTodoStore.tsx
+++ src/hooks/useTodoStore.tsx
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 const defaultTodos: Todo[] = [
   {
@@ -36,22 +37,29 @@ interface TodoStore {
   clearCompleted: () => void;
 }
 
-export const useTodoStore = create<TodoStore>((set, get) => ({
-  todos: defaultTodos,
-  filter: "all",
-  setFilter: (value) => set({ filter: value }),
-  addTodo: (title) => {
-    const newTodo: Todo = { id: uuidv4(), title, isCompleted: false };
+export const useTodoStore = create<TodoStore>()(
+  persist(
+    (set, get) => ({
+      todos: defaultTodos,
+      filter: "all",
+      setFilter: (value) => set({ filter: value }),
+      addTodo: (title) => {
+        const newTodo: Todo = { id: uuidv4(), title, isCompleted: false };
 
-    set({ todos: [...get().todos, newTodo] });
-  },
-  toggleTodo: (todoId) =>
-    set({
-      todos: get().todos.map((todo) =>
-        todoId === todo.id ? { ...todo, isCompleted: !todo.isCompleted } : todo
-      ),
+        set({ todos: [...get().todos, newTodo] });
+      },
+      toggleTodo: (todoId) =>
+        set({
+          todos: get().todos.map((todo) =>
+            todoId === todo.id
+              ? { ...todo, isCompleted: !todo.isCompleted }
+              : todo
+          ),
+        }),
+      clearCompleted: () => {
+        set({ todos: get().todos.filter((todo) => !todo.isCompleted) });
+      },
     }),
-  clearCompleted: () => {
-    set({ todos: get().todos.filter((todo) => !todo.isCompleted) });
-  },
-}));
+    { name: "todosStorage" }
+  )
+);
